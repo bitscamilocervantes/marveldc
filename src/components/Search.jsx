aacf45d0383f9ea7a2d6ec19794a6f561a896373
Camilo Cervantes <cacesa8931@gmail.com>
2020-05-26 15:40:01 -0500

    search marvel character

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 import './styles/search.scss';
 
 
-function Search(){
+function Search(props){
+    const { onUpdateCharacters } = props;
+    const handleChange = value => {
+        onUpdateCharacters(value);
+      };    
     return(
         <section className="main">
-            <input type="text" className="input" placeholder="Search character..." />
+            <input type="text" className="input" placeholder="Search character..."
+            onChange={e => handleChange(e.target.value)} />
         </section>
     )
 }
