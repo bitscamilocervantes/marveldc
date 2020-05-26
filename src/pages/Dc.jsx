adb3191d72ca04b5867c54c4e70bcff6ee06f589
Camilo Cervantes <cacesa8931@gmail.com>
2020-05-26 15:43:48 -0500

    search dc characters

--- src/pages/Dc.jsx
+++ src/pages/Dc.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Search from '../components/Search';
 import Characters from '../components/Characters';
@@ -10,13 +10,40 @@ const endpoint = "http://localhost:3000/dc";
 
 function Dc(){
     const characters = useInitialState(endpoint);
+    const [charactersList, setCharactersList] = useState([]);
+
+    const excludeColumns = ['image', 'url', 'biography', 'power'];
+    const filterData = (value) => {
+        const lowercasedValue = value.toLowerCase().trim();
+        if (lowercasedValue === '' || lowercasedValue.length <= 2) {
+          setCharactersList(characters);
+        } else {
+          if (lowercasedValue.length > 2) {
+            const filteredData = characters.filter((item) => {
+              return Object.keys(item).some((key) =>
+                excludeColumns.includes(key) ? false : item[key].toString().toLowerCase().includes(lowercasedValue)
+              );
+            });
+            setCharactersList(filteredData);
+          }
+        }
+      };
+
+      useEffect(() => {
+        setCharactersList(characters);
+      }, [characters]);
     return (
         <React.Fragment>
-            <Search />
+            <Search onUpdateCharacters={(query) => filterData(query)} />
             <Characters studio="dc">
-                {characters.map((item, i) => (
+            {charactersList.length > 0 && (
+                charactersList.map((item, i) => (
                     <Character key={i} {...item} tipo="dc" />
-                ))} 
+                ))
+            )}
+            {charactersList.length == 0 && (
+                <div className="msg-not-found">No characters found!</div>
+            )}      
             </Characters>
         </React.Fragment>
     );
